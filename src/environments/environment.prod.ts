f347ac424392c8ebaba1e9c3a6ad155720e337fa
ToastedFlaco <aaron.arredondoar@outlook.com>
2020-10-26 17:08:31 -0600

    cambios en el entorno de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDcJYW73Nzlp8hngan2PSShTd3FM9YbRWo",
+    authDomain: "internacionalizacionfime.firebaseapp.com",
+    databaseURL: "https://internacionalizacionfime.firebaseio.com",
+    projectId: "internacionalizacionfime",
+    storageBucket: "internacionalizacionfime.appspot.com",
+    messagingSenderId: "435331321596",
+    appId: "1:435331321596:web:c37b39fa40712a047b9179",
+    measurementId: "G-2K6EX46V7R"
+  }
 };
